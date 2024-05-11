9efd6ea09b41150c6162db541ff39d939992e8f0
abtblk <137126638+abtblk@users.noreply.github.com>
2024-05-11 02:57:05 +0000

    Fixed white border around tab

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function openTab(url) {
     var win = window.open('', '_blank');
     if (win) {
         // Browser has allowed it to be opened
-        win.document.write('<iframe src="' + url + '" style="width:100%; height:100%; border:none;", width="100%" height="100%></iframe>');
+        win.document.write('<style>body { margin: 0; padding: 0; }</style><iframe src="' + url + '" style="width:100%; height:100%; border:none;"></iframe>');
     } else {
         // Browser has blocked it
         alert('Please allow popups for this website');
