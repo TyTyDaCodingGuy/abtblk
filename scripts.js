3a122505e8ece7ad515c6d33ede01cafe1db23b7
abtblk <137126638+abtblk@users.noreply.github.com>
2024-05-11 02:11:14 +0000

    commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,10 @@
-
 function openTab(url) {
-    var win = window.open('about:blank');
-    var doc = win.document;
-    doc.open();
-    doc.write('<iframe src="' + url + '" style="width:100%; height:100%; border:none;"></iframe>');
-    doc.close();
+    var win = window.open('', '_blank');
+    if (win) {
+        // Browser has allowed it to be opened
+        win.document.write('<iframe src="' + url + '" style="width:100%; height:100%; border:none;"></iframe>');
+    } else {
+        // Browser has blocked it
+        alert('Please allow popups for this website');
+    }
 }
